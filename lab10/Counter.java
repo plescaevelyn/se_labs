325ce9a2fdc3f4a5d3e8eaf76d18f2af8b724c46
plescaevelyn <62516621+plescaevelyn@users.noreply.github.com>
2022-05-11 16:34:55 +0300

    Laboratories 11 and 12

---
+++ lab10/Counter.java
@@ -0,0 +1,30 @@
+package lab10;
+
+public class Counter extends Thread {
+	 
+    Counter(String name){
+          super(name);
+    }
+
+    public void run(){
+          for(int i=0;i<20;i++){
+                System.out.println(getName() + " i = "+i);
+                try {
+                      Thread.sleep((int)(Math.random() * 1000));
+                } catch (InterruptedException e) {
+                      e.printStackTrace();
+                }
+          }
+          System.out.println(getName() + " job finalised.");
+    }
+
+    public static void main(String[] args) {
+          Counter c1 = new Counter("counter1");
+          Counter c2 = new Counter("counter2");
+          Counter c3 = new Counter("counter3");
+
+          c1.start();
+          c2.start();
+          c3.start();
+    }
+}
